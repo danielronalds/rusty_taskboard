2ef0cb03e8f78100e326cc7eba0adad3d157b691
Daniel R <nzdanron@gmail.com>
2023-06-19 23:08:41 +1200

    Updated how tasks look in the list
    Tasks now each have their own little box

--- src/app/list.rs
+++ src/app/list.rs
@@ -1,6 +1,6 @@
 //! This module contains the logic for the list windows
 
-use egui::{Context, Ui};
+use egui::{containers::Frame, style::Margin, Color32, Context, Rounding, Ui};
 
 use crate::task::{List, Task};
 
@@ -11,24 +11,44 @@ pub fn draw_list(ctx: &Context, list: List) -> List {
     egui::Window::new("Tasklist")
         .resizable(false)
         .show(ctx, |ui| {
-            list = list.clone().into_iter() // The clone is needed here due to the closure
-                .map(|task| {
-                    ui.add_space(SPACE_BETWEEN_TASKS);
-                    draw_task(ui, task)
-                })
+            list = list
+                .clone() // The clone is needed here due to the closure
+                .into_iter()
+                .map(|task| draw_task(ui, task))
                 .collect();
         });
     list
 }
 
+const TASK_OUTER_MARGIN: f32 = 5.0;
+const TASK_INNER_MARGIN: f32 = 10.0;
+const TASK_ROUNDING: f32 = 5.0;
+const TASK_BORDER_WIDTH: f32 = 1.0;
+const TASK_WIDTH: f32 = 200.0;
+
 fn draw_task(ui: &mut Ui, task: Task) -> Task {
     let mut task = task;
 
-    ui.separator();
-    let title = task.title();
-    ui.checkbox(task.mut_completed(), title);
-    ui.label("This is the begining of a task");
-    ui.separator();
+    Frame::none()
+        .fill(Color32::LIGHT_GRAY)
+        .outer_margin(Margin::same(TASK_OUTER_MARGIN))
+        .rounding(Rounding::same(TASK_ROUNDING))
+        .show(ui, |ui| {
+            Frame::none()
+                .outer_margin(Margin::same(TASK_BORDER_WIDTH))
+                .inner_margin(Margin::same(TASK_INNER_MARGIN))
+                .rounding(Rounding::same(TASK_ROUNDING))
+                .fill(Color32::WHITE)
+                .show(ui, |ui| {
+                    ui.set_width(TASK_WIDTH);
+                    let title = task.title();
+                    ui.checkbox(task.mut_completed(), title);
+                    let description = task.description();
+                    if !description.is_empty() {
+                        ui.label(description);
+                    }
+                });
+        });
 
     task
 }
